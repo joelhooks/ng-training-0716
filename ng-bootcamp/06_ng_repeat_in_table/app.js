8411236b31f9377433a3d0df1077434872ac5c86
joelhooks <joelhooks@gmail.com>
2016-07-13 10:16:45 -0500

    basic repeating of data with ng-repeat :+1:

--- ng-bootcamp/06_ng_repeat_in_table/app.js
+++ ng-bootcamp/06_ng_repeat_in_table/app.js
@@ -1,5 +1,36 @@
 angular.module('exampleApp', [])
   .controller('AppCtrl', function AppCtrl() {
     var app = this;
+
+    app.people = [
+      {
+        name:'Henry',
+        title: 'Janitor'
+      },
+      {
+        name:'Sally',
+        title: 'Manager'
+      },
+      {
+        name:'Joel',
+        title: 'Painter'
+      },
+      {
+        name:'Mike',
+        title: 'Intern'
+      },
+      {
+        name:'Becky',
+        title: 'Engineer'
+      },
+      {
+        name:'Jess',
+        title: 'Intern'
+      }
+    ];
+
+    app.addNewPerson = function(name) {
+      app.people.push({name: name});
+    }
   })
 ;
